314189a8742ad8d3be6e64b7d991dbc86e5753c7
Rishabh <jakhmolarishabh189@gmail.com>
2025-10-31 07:02:00 +0000

    conversion of datatypes

---
+++ 02_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score= "33abc"
+//console.log(typeof(score));
+ 
+let valueInNumber = Number(score);
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc"=> NaN
+//true=> 1;
+//false=>0
+
+let isLoggedIn= "rishabh";
+let booleanisLiggedIn= Boolean(isLoggedIn);
+//console.log(booleanisLiggedIn)
+
+
+// 1+. true; 0=>false
+//""=> false
+//"rishabh"=>true
+
+let someNumber =33
+ 
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
