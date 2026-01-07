70b3b53402f85141ac56df1c472f6a3f8023bb2c
Rishabh <jakhmolarishabh189@gmail.com>
2026-01-07 00:54:54 +0000

    dataType_sumary

--- 02_basics/04_comparison.js
+++ 02_basics/04_comparison.js
@@ -1,9 +1,12 @@
 // console.log("2">1);
 // console.log("02">1);
 
-// console.log(null>0);
-// console.log(null == 0);
-// console.log(null >=0);
+// console.log(null>0); //false
+// console.log(null == 0);//false
+// console.log(null >=0);//true
+
+//Reason=equality check and comparisons work differently
+//comparison convert null toa number, treat as 0.
 
 
 console.log(undefined ==0);
